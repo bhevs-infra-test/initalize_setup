80718f15f1ed562676d7ea18ba9bfee9d82e6240
choiseu <choiseu@bhevs.co.kr>
2025-12-02 17:07:08 +0900

    feat: apply, destroy 시 순차적으로 진행되도록 변경

--- github/destroy.sh
+++ github/destroy.sh
@@ -2,4 +2,14 @@
 set -e
 
 terraform init
-terraform destroy -auto-approve
+
+echo "==== [0] validate ===="
+terraform validate
+
+echo "==== [1/2] 레포/유저 등 삭제 ===="
+terraform destroy -auto-approve -target=module.repos -target=module.users
+
+echo "==== [2/2] 팀 삭제 ===="
+terraform destroy -auto-approve -target=module.project_teams
+
+echo "모든 리소스가 정상적으로 삭제되었습니다."
--- github/terraform-apply.sh
+++ github/terraform-apply.sh
@@ -2,14 +2,28 @@
 set -e
 
 terraform init
-terraform validate
-terraform plan -out=planfile
 
+echo "==== [0] validate ===="
+terraform validate
+
+echo "==== [1/2] 팀(plan) ===="
+terraform plan -target=module.project_teams -out=planfile.teams
 if [ $? -ne 0 ]; then
-  echo "Error: Terraform plan failed."
+  echo "Error: Terraform plan (teams) failed."
   exit 1
 fi
 
-terraform apply -auto-approve planfile
+echo "==== [1/2] 팀(apply) ===="
+terraform apply -auto-approve planfile.teams
 
-echo "생성되었습니다."
+echo "==== [2/2] 나머지(plan) ===="
+terraform plan -out=planfile.rest
+if [ $? -ne 0 ]; then
+  echo "Error: Terraform plan (rest) failed."
+  exit 1
+fi
+
+echo "==== [2/2] 나머지(apply) ===="
+terraform apply -auto-approve planfile.rest
+
+echo "모든 리소스가 정상적으로 생성되었습니다."
